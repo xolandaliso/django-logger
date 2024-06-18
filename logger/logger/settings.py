858d7e8b8e13a2e969fb010d14bc83b79f786e38
xola <xola.ndaliso@gmail.com>
2024-06-18 15:03:46 +0200

    modified the forms and models.py files

--- logger/logger/settings.py
+++ logger/logger/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
