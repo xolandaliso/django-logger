0e45d6313645e45b8e58c9b8579901eb3ead346f
xola <xola.ndaliso@gmail.com>
2024-06-24 21:20:40 +0200

    added views for ticked update and delete. working on creating a comment box between user and responsible employee for the ticket...

--- logger/tickets/urls.py
+++ logger/tickets/urls.py
@@ -14,6 +14,14 @@ urlpatterns = [
     path('department_selection/', views.department_selection, name='department_selection'),
     path('department_ticket_creation/', views.department_ticket_creation, name='department_ticket_creation'),
     path('manage_tickets/', views.manage_tickets, name='manage_tickets'),
+    path('ticket/<int:pk>/update/', views.ticket_update, name='ticket_update'),
+    path('ticket/<int:pk>/delete/', views.ticket_delete, name='ticket_delete'),
     path('logout/', views.logout_view, name='logout'),
 
 ]
+
+htmx_urlpatterns = [
+    path('get_manage_tickets/', views.get_manage_tickets, name='get_manage_tickets'),
+]
+
+urlpatterns += htmx_urlpatterns
