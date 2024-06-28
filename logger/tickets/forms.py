f35042f2791bb59c31fd41c48a8b0db5729a100e
xola <xola.ndaliso@gmail.com>
2024-06-28 14:21:25 +0200

    added document field in the TicketForm

--- logger/tickets/forms.py
+++ logger/tickets/forms.py
@@ -35,6 +35,46 @@ class CustomUserForm(UserCreationForm):
         return user
 
 class TicketForm(forms.ModelForm):
+
+    document = forms.FileField(label='Attach Document', required=False)  # add a file upload field
+
+    class Meta:
+        model = Ticket
+        fields = ['ticket_description', 'ticket_type', 'location', 'employee']
+
+    def __init__(self, *args, **kwargs):
+        department = kwargs.pop('department', None)
+        super(TicketForm, self).__init__(*args, **kwargs)
+        if department:
+            self.fields['employee'].queryset = Employee.objects.filter(department=department)
+            self.fields['ticket_type'].queryset = Type.objects.filter(department=department)
+
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        if self.instance.pk:  # check if instance exists (i.e., for update)
+            self.helper.add_input(Submit('submit', 'Update Ticket', css_class='btn btn-primary'))
+        else:
+            self.helper.add_input(Submit('submit', 'Create Ticket', css_class='btn btn-primary'))
+        self.helper.layout = Layout(
+            Field('ticket_description', css_class='form-control'),
+            Field('ticket_type', css_class='form-control'),
+            Field('location', css_class='form-control'),
+            Field('employee', css_class='form-control'),
+            'document'  # add document field to form layout
+        )
+    def save(self, commit=True):
+        ticket = super(TicketForm, self).save(commit=False)
+        if commit:
+            ticket.save()
+
+        # Save the uploaded document if present
+        if self.cleaned_data.get('document'):
+            Documents.objects.create(ticket=ticket, document=self.cleaned_data['document'])
+
+        return ticket
+
+'''
+class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = [ 
@@ -55,17 +95,17 @@ class TicketForm(forms.ModelForm):
             Field('location', css_class='form-control'),
             Field('employee', css_class='form-control'),
         )
-
-class TicketFormComments(forms.ModelForm):
+'''
+class TicketCommentsForm(forms.ModelForm):
     class Meta:
         model = TicketComments
         fields = ['comment']
 
         def __init__(self, *args, **kwargs):
-            super(TicketFormComments, self).__init__(*args, **kwargs)
+            super(TicketCommentsForm, self).__init__(*args, **kwargs)
             self.helper = FormHelper()
             self.helper.form_method = 'post'
+            self.helper.add_input(Submit('submit', 'Add Comment', css_class='btn btn-secondary mt-2'))
             self.helper.layout = Layout(
-                Field('comment', css_class='form-control', rows=3),
-                Submit('submit', 'Add Comment', css_class='btn btn-primary mt-2')
+                Field('comment', css_class='form-control', rows=3)
         )
