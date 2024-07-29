e281e524a904caa6e9c27fcb5a932fecca39e2df
xola <xola.ndaliso@gmail.com>
2024-07-29 09:47:55 +0200

    Update settings.py

--- logger/logger/settings.py
+++ logger/logger/settings.py
@@ -141,12 +141,4 @@ MESSAGE_TAGS = {
 }
 
 #email notification settings
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'xola.ndaliso@gmail.com' 
-EMAIL_HOST_PASSWORD = 'slde ooav qmpr nbwi'
-DEFAULT_FROM_EMAIL = 'xola.ndaliso@gmail.com'  
-EMAIL_FROM_NAME = 'TAD Ticket Portal'  
+#hidden 
