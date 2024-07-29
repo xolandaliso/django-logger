3fe43baf99850c083852761924f158e757ddc1e8
xola <xola.ndaliso@gmail.com>
2024-07-29 09:23:46 +0200

    attempted fixing ticket status to call human readable values on the UI

--- logger/tickets/admin.py
+++ logger/tickets/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.db import models
 from .models import Department, Employee, Ticket, Status, Type, Resolution
 
 # Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    # ... other admin configurations ...
+
+    formfield_overrides = {
+        models.ForeignKey: {'queryset': Status.objects.all()},
+    }
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Status)
 admin.site.register(Department)
 admin.site.register(Employee)
-admin.site.register(Ticket)
-admin.site.register(Status)
+#admin.site.register(Ticket)
 admin.site.register(Type)
 admin.site.register(Resolution)
